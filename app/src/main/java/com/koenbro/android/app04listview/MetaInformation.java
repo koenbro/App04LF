50f85564bc8719f966eb7974a065981bc8805bd1
lvaszar <lvaszar@yahoo.com>
2015-02-26 07:14:17 -0700

    Javadoc documentation of classes.

---
+++ app/src/main/java/com/koenbro/android/app04listview/MetaInformation.java
@@ -0,0 +1,83 @@
+package com.koenbro.android.app04listview;
+
+import android.text.format.Time;
+import android.widget.Toast;
+
+/**
+ * Meta information for shot
+ * @author laszlo
+ * @date 2/26/15.
+ */
+public class MetaInformation {
+    private String day;
+    private String time;
+    private Time now;
+    private GPSTracker gps;  // GPSTracker class
+    private double latitude;
+    private double longitude;
+
+    public MetaInformation() {
+        now = new Time(Time.getCurrentTimezone());
+        gps = new GPSTracker(ApplicationContextProvider.getContext());
+    }
+
+    private void timeStamp(){
+        now.setToNow();
+        day =  now.year +"-"+(now.month+1)+"-"+now.monthDay;
+        time = now.format("%k:%M:%S");
+    }
+
+    private void locationStamp() {
+        // check if GPS enabled
+        if (gps.canGetLocation()) {
+            latitude = gps.getLatitude();
+            longitude = gps.getLongitude();
+
+            // \n is for new line
+            Toast.makeText(ApplicationContextProvider.getContext(), "Your Location is - \nLat: " +
+                    latitude + "\nLong: " + longitude, Toast.LENGTH_SHORT).show();
+        } else {
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gps.showSettingsAlert();
+        }
+    }
+
+    /**
+     * Obtain and return the current day
+     * @return  day  formatted as y-m-d
+     */
+    public String getDay() {
+        timeStamp(); //need to obtain fresh time
+        return day;
+    }
+
+    /**
+     * Obtain and return the current time
+     * @return  time    formatted as h:m:s
+     */
+    public String getTime() {
+        timeStamp(); //need to obtain fresh time
+        return time;
+    }
+
+    /**
+     * Obtain a fresh location and return the latitude
+     * @return  double  latitude
+     */
+    public double getLatitude() {
+        locationStamp();
+        return latitude;
+    }
+
+    /**
+     * Obtain a fresh location and return the longitude
+     * @return  double  longitude
+     */
+    public double getLongitude() {
+        locationStamp();
+        return longitude;
+    }
+
+}
